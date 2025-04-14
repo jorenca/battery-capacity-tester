95fba24a988e4acd2b848c99499f8498a69b7c83
jorenca <mailnamefrustration@gmail.com>
2025-04-14 17:03:32 +0300

    Add debounced start/stop button

--- capacity-tester/capacity-tester.ino
+++ capacity-tester/capacity-tester.ino
@@ -30,12 +30,15 @@ void beep(unsigned int hz, unsigned int milliseconds) {
 }
 
 
-/////////////////////// Rotary encoder ///////////////////////
+/////////////////////// Start/Stop Button + Rotary encoder ///
 
 #define ROTARY_ENCODER_A_PIN 35
 #define ROTARY_ENCODER_B_PIN 33
 #define ROTARY_ENCODER_BUTTON_PIN 14
 #define ROTARY_ENCODER_STEPS 2
+
+#define START_STOP_BUTTON_PIN 17
+
 
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(
   ROTARY_ENCODER_A_PIN,
@@ -49,6 +52,36 @@ AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(
 void IRAM_ATTR readEncoderISR() {
     rotaryEncoder.readEncoder_ISR();
 }
+
+
+#define DEBOUNCE_MILLIS 500
+unsigned long volatile lastStartStopButtonPressTime = 0;
+unsigned long volatile lastStartStopButtonReleaseTime = 0;
+volatile bool startStopButtonPressed = false;
+
+void IRAM_ATTR startStopButtonPressed_ISR() {
+
+  unsigned long nowMillis = millis();
+  bool newIsPressed = !digitalRead(START_STOP_BUTTON_PIN);
+  
+  if (newIsPressed) {
+    unsigned long lastButtonPress = lastStartStopButtonPressTime;
+    lastStartStopButtonPressTime = nowMillis;
+    if (nowMillis - lastButtonPress < DEBOUNCE_MILLIS) return;
+    if (nowMillis - lastStartStopButtonReleaseTime < DEBOUNCE_MILLIS) return;
+    
+    startStopButtonPressed = true;
+  } else {
+    lastStartStopButtonReleaseTime = nowMillis;
+  }
+}
+
+bool wasStartStopPressed() {
+  bool retval = startStopButtonPressed;
+  startStopButtonPressed = false;
+  return retval;
+}
+
 
 
 /////////////////////// LCD display //////////////////////////
@@ -91,13 +124,27 @@ void updateScreen() {
 }
 
 
-/////////////////////// LCD display //////////////////////////
+/////////////////////// Current control //////////////////////
+
+#define CURRENT_SET_DAC_PIN 25
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void setDrawCurrent(float amps) {
+    byte dacValue = floor(mapfloat(amps, 0, 3.3, 0, 255)); // [0, 255] range
+    dacWrite(CURRENT_SET_DAC_PIN, dacValue);
+}
+
+
+/////////////////////// Settings input ///////////////////////
 
 void settingsSelect(int index) {
   currentSetting = index;
 
   if (currentSetting == 0) { // set maxI
-    rotaryEncoder.setBoundaries(1, 33, false); //minValue, maxValue, circleValues true|false (when max go to min and vice versa)
+    rotaryEncoder.setBoundaries(1, 30, false); //minValue, maxValue, circleValues true|false (when max go to min and vice versa)
     rotaryEncoder.setEncoderValue(maxI * 10);
     rotaryEncoder.disableAcceleration();
   } else { // set minU
@@ -113,6 +160,8 @@ void settingsSelect(int index) {
 void settingsUpdate() {
     if (currentSetting == 0) { // set maxI
     maxI = rotaryEncoder.readEncoder() / 10.0;
+    
+    setDrawCurrent(maxI); // FIXME FOR TESTING ONLY
   } else { // set minU
     minU = rotaryEncoder.readEncoder() / 10.0;
   }
@@ -122,13 +171,19 @@ void settingsUpdate() {
 }
 
 
+
 /////////////////////// Arduino procedures ///////////////////
 
 void setup() {
   Serial.begin(115200);
+
+  setDrawCurrent(0);
   
   lcd.begin(16, 4);
   lcd.backlight();
+
+  pinMode(START_STOP_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(START_STOP_BUTTON_PIN, startStopButtonPressed_ISR, CHANGE);
   
   pinMode(ROTARY_ENCODER_A_PIN, INPUT_PULLUP);
   pinMode(ROTARY_ENCODER_B_PIN, INPUT_PULLUP);
@@ -149,5 +204,10 @@ void loop() {
     int newSetting = (currentSetting + 1) % 2;
     settingsSelect(newSetting);
   }
+
+  if (wasStartStopPressed()) {
+    beep(1650, 1000);
+  }
+
 
 }
